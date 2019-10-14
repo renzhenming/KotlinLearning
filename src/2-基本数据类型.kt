5d32a08b87b4cd201ed58cc7ebe5e443effa6d07
renzhenming <1565766177@qq.com>
2019-10-15 07:51:42 +0800

    init

---
+++ src/2-基本数据类型.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    var b:Boolean = false   //=1byte=8bit
+    println(b)
+    var byte:Byte = 10  //=1byte=8bit
+    println(byte)
+    var char:Char = 'a'  //=2byte=16bit
+    println(char)
+    var short:Short = 10  //=2byte=16bit
+    println(short)
+    var int:Int = 20  //=4byte=32bit
+    println(int)
+    var float:Float = 3.1415926f //=4byte=32bit 精确到小数点后6位
+    println(float)
+    var long:Long = 30 //=8byte=64bit
+    println(long)
+    var double:Double = 3.1415926 //=8byte=64bit 精确到小数点后15到16位
+    println(double)
+
+
+}
